3c17cbc1e98c3c78b2d8157fa6a63afe3e9cd530
MOHMAD SABER <100026144+SaberShareef@users.noreply.github.com>
2023-08-19 22:54:35 +0530

    Add files via upload
    SIMPLE ARITHMATIC OPRATTIONS

---
+++ Arithmatic_Opearatoion.java
@@ -0,0 +1,24 @@
+package My_Programs;
+
+public class Arithmatic_Opearatoion
+{
+	
+	public static void main(String args[])
+	{
+		
+		static int a=25;
+		static int b=20;
+		
+		int sum=a+b;
+		int sub=a-b;
+		int mul=a*b;
+		int div=a/b;
+				
+		System.out.println("addition of a and b is =" + sum);
+		System.out.println("substraction of a and b is =" + sub);
+		System.out.println("multiplication of a and b is =" + mul);
+		System.out.println("divition of a and b is =" + div);
+		System.out.println(a);
+	}
+
+}
